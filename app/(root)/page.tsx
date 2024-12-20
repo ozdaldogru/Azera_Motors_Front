996d71f12bd4f21fb63da6618b13b7b565ace5d4
Ozdal Dogru <ozdaldogru@gmail.com>
2024-12-19 21:24:36 -0500

    google maps location updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,13 @@
 import ProductList from "@/components/ProductList";
 
 
+
 export default function Home() {
   return (
     <>
 
       <ProductList />
+
 
     </>
   );
--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -6,13 +6,11 @@ function GoogleMaps() {
 
   return (
     <div>
-            <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.853536580223!2d-79.63425962330264!3d43.58876697110534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b470344a245b9%3A0x2442bc057c6637a!2s3525%20Kariya%20Dr%2C%20Mississauga%2C%20ON%20L5B%200C2!5e0!3m2!1sen!2sca!4v1734490072663!5m2!1sen!2sca" 
-            width="400" 
-            height="300" 
-            loading="lazy" 
-        >
 
+            <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.4431522898763!2d-79.74377257330227!3d43.597313821104756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b6b372b563031%3A0x31c107002c776595!2s6610%20Turner%20Valley%20Rd!5e0!3m2!1sen!2sca!4v1734636497998!5m2!1sen!2sca" 
+            width="600" height="450"  
+            loading="lazy" >
             </iframe>
     </div>
   );
