8ae96f8de0bcb05eb3c63cfbd3a060909fc7bbdc
Ozdal Dogru <ozdaldogru@gmail.com>
2024-12-23 19:31:23 -0500

    Enhance ProductList layout with flexbox for better alignment and spacing

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -5,12 +5,12 @@ const ProductList = async () => {
   const products = await getProducts();
 
   return (
-    <div >
+    <div className="flex flex-col items-center gap-10 py-8 px-5">
       <p className="text-heading1-bold">Products</p>
       {!products || products.length === 0 ? (
         <p className="text-body-bold">No products found</p>
       ) : (
-        <div>
+        <div className="flex flex-wrap justify-center gap-16 ">
           {products.map((product: ProductType) => (
             <ProductCard key={product._id} product={product}/>
           ))}
