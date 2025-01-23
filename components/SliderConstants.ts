a5edfa326ab0ba56154ce237f3e0011e652030ac
Ozdal Dogru <ozdaldogru@gmail.com>
2025-01-23 17:23:16 -0500

    Update slider titles for improved messaging and engagement

--- components/SliderConstants.ts
+++ components/SliderConstants.ts
@@ -8,14 +8,14 @@ export const images = [
     {
       id: 2,
       src: "/banner2.jpg",
-      title: "Azera Motors",
-      title2: "Best In Mississauga",
+      title: "We Have",
+      title2: "The Best Affordable Cars",
     },
     {
       id: 3,
       src: "/banner.png",
       title: "Come and Visit Us",
-      title2: "And Get Your New Car",
+      title2: "And Get Your New Car Today",
     },
   ];
   
