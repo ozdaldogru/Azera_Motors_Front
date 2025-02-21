07fac7a7c95e7a0a8c7520b843a8c022bdb52e87
Ozdal Dogru <ozdaldogru@gmail.com>
2025-02-20 23:14:50 -0500

    Remove PdfModal component and refactor Summary component to eliminate PDF viewing functionality

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,59 +1,51 @@
-"use client"
-
-import { useState } from 'react';
-import Image from 'next/image';
-import PdfModal from './PdfModal';
+import Image from "next/image";
 
 const Summary = ({ productInfo }: { productInfo: ProductType }) => {
-  const [isPdfModalOpen, setIsPdfModalOpen] = useState(false);
 
-  const handleOpenPdfModal = () => {
-    setIsPdfModalOpen(true);
-  };
 
-  const handleClosePdfModal = () => {
-    setIsPdfModalOpen(false);
-  };
+    return (
 
-  return (
-    <div className="flex flex-col border-t">
-      <div className="text-[50px] max-[431px]:text-[30px] flex flex-row justify-between items-center gap-2 max-[564px]:flex-col">
-        <div className="flex flex-row items-center gap-2">
-          <p className="text-[#424A5]">{productInfo.make}</p>
+        <div className="flex flex-col border-t"> 
+
+
+        
+       <div className="text-[50px] max-[431px]:text-[30px] flex flex-row justify-between items-center gap-2 max-[564px]:flex-col">
+            <div className="flex flex-row items-center gap-2">
+            <p className="text-[#424A5]">{productInfo.make}</p>
+            </div>
+
+            <div className="flex flex-row items-center gap-2 max-[431px]:text-[25px] max-[431px]:flex-col text-[#6A7281]">
+            <p >${productInfo.price.toLocaleString()} </p> <span className="text-[20px] max-[431px]:text-[15px]">+ HST & Licensing</span>
+            </div>
         </div>
 
-        <div className="flex flex-row items-center gap-2 max-[431px]:text-[25px] max-[431px]:flex-col text-[#6A7281]">
-          <p>${productInfo.price.toLocaleString()}</p>
-          <span className="text-[20px] max-[431px]:text-[15px]">+ HST & Licensing</span>
+        <div className="text-[20px] flex flex-row gap-2 items-center gap-2 max-[564px]:justify-center max-[431px]:text-[15px]">
+   
+            
+            <p className="text-[#8F95A0]">{productInfo.year}</p>
+            
         </div>
-      </div>
 
-      <div className="text-[20px] flex flex-row gap-2 items-center gap-2 max-[564px]:justify-center max-[431px]:text-[15px]">
-        <p className="text-[#8F95A0]">{productInfo.year}</p>
-      </div>
+        <div className="flex flex-row justify-between text-center gap-2 w-full py-2 max-[564px]:items-center max-[564px]:flex-col">
 
-      <div className="flex flex-row justify-between text-center gap-2 w-full py-2 max-[564px]:items-center max-[564px]:flex-col">
-        {productInfo.history && productInfo.history.length > 0 ? (
-         <Image onClick={handleOpenPdfModal} src="/carfax.svg" alt="carfax car history logo" width={115} height={50} style={{ height: "auto" }}/>
-          
-        ) : (
-          <p>No history available</p>
-        )}
-        {productInfo.lowmileage === 'Yes' || productInfo.lowmileage === 'yes' ? (
-          <button className="bg-[#8F95A0] w-[115px] text-white py-2 px-4 border rounded">Low KM</button>
-        ) : (
-          ''
-        )}
-        {productInfo.numberofowner === 1 || productInfo.numberofowner < 2 ? (
-          <button className="bg-[#8F95A0] w-[115px] text-white py-2 px-4 border rounded">{productInfo.numberofowner} Owner</button>
-        ) : (
-          ''
-        )}
-      </div>
+                {productInfo.history === "" ? ("") : (  
+                    <a href={productInfo.history}><Image src="/carfax.svg" alt="carfax car history logo" width={115} height={50} style={{ height: "auto" }}/></a>
+               
+                
+                )}
+                {productInfo.lowmileage === "Yes" || productInfo.lowmileage === "yes" ? (
+                <button className="bg-[#8F95A0]  w-[115px] text-white py-2 px-4 border rounded">Low KM</button>
+                ) : ("")}
 
-      <PdfModal pdfUrl={productInfo.history} isOpen={isPdfModalOpen} onClose={handleClosePdfModal} />
-    </div>
-  );
-};
+                {productInfo.numberofowner === 1 || productInfo.numberofowner < 2 ? (
+                <button className="bg-[#8F95A0] w-[115px] text-white py-2 px-4 border  rounded">{productInfo.numberofowner} Owner</button>
+                ) : ("")}
 
-export default Summary;
+        </div>
+
+        
+        
+        </div>
+    )}
+
+    export default Summary;
