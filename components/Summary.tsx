3ad227470a326aa950e4edef689265cc569697df
Ozdal Dogru <ozdaldogru@gmail.com>
2025-08-04 18:52:21 -0400

    Refactor Summary component layout for improved responsiveness and styling consistency

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -5,28 +5,28 @@ const Summary = ({ productInfo }: { productInfo: ProductType }) => {
 
     return (
 
-        <div className="flex flex-col border-t"> 
+        <div className="flex flex-col border-t "> 
 
 
         
-       <div className="text-[50px] max-[431px]:text-[30px] flex flex-row justify-between items-center gap-2 max-[564px]:flex-col">
-            <div className="flex flex-row items-center gap-2">
-            <p className="text-[#424A5]">{productInfo.make}</p>
+       <div className=" flex flex-row justify-between items-center gap-2 max-[975px]:flex-col max-[975px]:justify-center ">
+            <div className="flex flex-row items-center gap-2 text-[50px] max-[450px]:text-[45px] max-[410px]:text-[40px] max-[369px]:text-[35px]">
+            <p className="text-[#424A5]">{productInfo.make} {productInfo.model}</p>
             </div>
 
-            <div className="flex flex-row items-center gap-2 max-[431px]:text-[25px] max-[431px]:flex-col text-[#6A7281]">
+            <div className="flex flex-row items-center gap-2 text-[45px] max-[450px]:text-[35px] max-[369px]:text-[32px] max-[431px]:flex-col text-[#6A7281]">
             <p >${productInfo.price.toLocaleString()} </p> <span className="text-[20px] max-[431px]:text-[15px]">+ HST & Licensing</span>
             </div>
         </div>
 
-        <div className="text-[20px] flex flex-row gap-2 items-center gap-2 max-[564px]:justify-center max-[431px]:text-[15px]">
+        <div className="text-[20px] flex flex-row gap-4 items-center py-4 max-[975px]:flex-col max-[564px]:justify-center max-[431px]:text-[15px]">
    
             
             <p className="text-[#8F95A0]">{productInfo.year}</p>
             
         </div>
 
-        <div className="flex flex-row justify-between text-center gap-2 w-full py-2 max-[564px]:items-center max-[564px]:flex-col">
+        <div className="text-[20px] flex flex-row gap-4 items-center gap-4 max-[975px]:flex-col max-[564px]:justify-center max-[431px]:text-[15px]">
 
                 {productInfo.history === "" ? ("") : (  
                     <a href={productInfo.history}><Image src="/carfax.svg" alt="carfax car history logo" width={115} height={50} style={{ height: "auto" }}/></a>
