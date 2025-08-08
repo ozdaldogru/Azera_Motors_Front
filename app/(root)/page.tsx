f37037914a9da3a2cc690fc9dc9682eb3756c17d
Ozdal Dogru <ozdaldogru@gmail.com>
2025-08-07 22:16:44 -0400

    Add Google Tag Manager scripts to Head component for analytics tracking

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,28 @@
 import Slider from "@/components/Slider";
 import ProductListFiltered from "@/components/ProductListFiltered";
 import CustomCookieConsent from "@/components/CustomCookieConsent";
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
     <>
       <div>
+
+      <Head>
+        <script async src={process.env.YOUR_GOOGLE_TAG_SCRIPT_URL}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZD6CDE1XEZ');
+            `,
+          }}
+        />
+      </Head>
+
         <div className="w-full" >
           <Slider />
         </div>
