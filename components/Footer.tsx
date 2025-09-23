87b4d9996d74e649ed484aea90ab423dbec64102
Ozdal Dogru <ozdaldogru@gmail.com>
2025-09-22 20:30:10 -0400

    Refactor Footer and GoogleMaps components for improved structure and readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,96 +4,84 @@ function Footer() {
     return (
       <footer className="bg-[#d4dce2] flex flex-col justify-between items-center gap-10 py-8 px-5 object-fill">
         
-      <div className="flex flex-wrap justify-center gap-16 max-[431px]:gap-4">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-[431px]:gap-4 justify-items-center">
 
-            <div className="">
+  {/* Operation Hours */}
+  <div className="w-full max-w-xs">
+    <h2 className="text-[24px] text-bold text-center underline underline-offset-4 mb-5 max-[431px]:text-[20px]">Operation Hours:</h2>
+    <ul className="flex flex-col px-2">
+      <li className="flex flex-col text-center">
+        <p className="text-green-900 font-bold">Monday- Saturday:</p>
+        <p className="text-green-900">09:00a.m.-08:00p.m.</p>
+      </li>
+      <li className="flex flex-col text-center">
+        <p className="text-green-900 font-bold">Sunday:</p>
+        <p className="text-green-900">09:00a.m.-03:00p.m.</p>
+      </li>
+    </ul>
+  </div>
 
-              <h2 className=" text-[24px] justify-center gap-2 text-bold text-center underline underline-offset-4 mb-5 max-[431px]:text-[20px]">Operation Hours:</h2>
-           
-              <ul className="flex flex-col px-02 px-4">
-                <li className=" flex flex-col text-center">
-                  <p className="text-green-900-bold transition " > Monday- Saturday: </p>
-                  <p className="text-green-900-bold transition  justify-right" > 09:00a.m.-08:00p.m. </p>
-                </li>
+  {/* Contact Us */}
+  <div className="w-full max-w-xs">
+    <h2 className="text-[24px] text-bold underline underline-offset-4 mb-5 max-[431px]:text-[20px] text-center">Contact Us:</h2>
+    <div className="flex flex-col justify-center text-[19px] max-[431px]:text-[15px]">
+      <a href="tel:14375056334" className="text-center text-black hover:text-green-700" target="_blank" rel="noopener noreferrer">+1-437-505-63-34</a>
+      <a href="mailto:info@azeramotors.ca" className="text-center text-black hover:text-red-700" target="_blank" rel="noopener noreferrer">info@azeramotors.ca</a>
+    </div>
 
+      <div className="w-full max-w-xs flex flex-col items-center">
+    <ul className="mt-6 flex justify-center gap-6 md:gap-8">
+      <li>
+        <a
+          href="https://www.facebook.com/profile.php?id=61571836432279"
+          rel="noreferrer"
+          target="_blank"
+          className="text-gray-700 transition hover:text-blue-700"
+        >
+          <span className="sr-only">Facebook</span>
+          <svg className="h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path
+              fillRule="evenodd"
+              d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/azeramotorsinc/"
+          rel="noreferrer"
+          target="_blank"
+          className="text-gray-700 transition hover:text-orange-700/75"
+        >
+          <span className="sr-only">Instagram</span>
+          <svg className="h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+            <path
+              fillRule="evenodd"
+              d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+  </div>
 
-                <li className=" flex flex-col text-center">
-                  <p className="text-green-900-bold transition "> Sunday: </p>
-                  <p className="text-green-900-bold transition " > 09:00a.m.-03:00p.m. </p>
-                </li>
-              </ul>
-
-            </div>
-            <div>
-            <div className="flex flex-col ">   
-                <h2 className=" text-[24px] justify-center gap-2 text-bold underline underline-offset-4 mb-5 max-[431px]:text-[20px] text-center">Contact Us:</h2>   
-                <div className="flex flex-col justify-center text-[19px] max-[431px]:text-[15px]">   
-                  <a href="tel:14375056334" className="text-center text-black  hover:text-green-700" target="blank">+1-437-505-63-34</a>    
-                  <a href="mailto:info@azeramotors.ca" className="text-center text-black hover:text-red-700" target="blank">info@azeramotors.ca</a>
- 
-              </div>
-
-            </div>
-            <br />
-
-            <div>
-
-            
-            <ul className="mt-6 flex justify-center gap-6 md:gap-8">
-              <li>
-                <a
-                  href="https://www.facebook.com/profile.php?id=61571836432279"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="text-gray-700 transition hover:text-blue-700"
-                >
-                  <span className="sr-only">Facebook</span>
-                  <svg className="h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                    <path
-                      fillRule="evenodd"
-                      d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </li>
-        
-              <li>
-                <a
-                  href="https://www.instagram.com/azeramotorsinc/"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="text-gray-700 transition hover:text-orange-700/75"
-                >
-                  <span className="sr-only">Instagram</span>
-                  <svg className="h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                    <path
-                      fillRule="evenodd"
-                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </a>
-              </li>
-               
-
-            </ul>
-            </div>
-
-            <br />
+  {/* Social Links */}
 
 
-            </div>
-            <div className="rounded-lg sm:h-[88px] sm:w-[88px]">
-              <GoogleMaps />
-            </div>
+  {/* Google Maps */}
+  <div className="w-full max-w-xs flex justify-center items-center">
+  <GoogleMaps />
+  </div>
+</div>
 
-      </div>
-
-          <div className=" flex flex-col justify-center text-center py-4">
+      <div className=" flex flex-col justify-center text-center py-4">
             <p className="border-b-2">All Rights Reserved  &copy; Azera Motors</p>
             <p >Developed by Ozdal Dogru</p>
-         </div>
+      </div>
   
     </footer>
     )
--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,24 +1,17 @@
-"use client"
+"use client";
 
-
-
-function GoogleMaps() {
-
-
-
-  return (
-    <div >
-
-        <iframe 
-          id="map" aria-label="Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.447822193178!2d-79.74380262309514!3d43.59721657110498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b6b5b3fd9049b%3A0xd17d3f74068c80d5!2sAzera%20Motors!5e0!3m2!1sen!2sca!4v1735272842888!5m2!1sen!2sca" 
-          width="300" 
-          height="200"
-          loading="lazy">
-            
-        </iframe>
-    </div>
-  );
-};
+const GoogleMaps = () => (
+  <iframe
+    title="Azera Motors Location"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.447822193178!2d-79.74380262309514!3d43.59721657110498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b6b5b3fd9049b%3A0xd17d3f74068c80d5!2sAzera%20Motors!5e0!3m2!1sen!2sca!4v1735272842888!5m2!1sen!2sca"
+    width="100%"
+    height="100%"
+    style={{ border: 0 }}
+    allowFullScreen={true}
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    className="rounded-lg w-full h-40 sm:h-32"
+  ></iframe>
+);
 
 export default GoogleMaps;
