ac7a0dfeac0582440eb4f3fa039dcd5afee5faf1
Ozdal Dogru <ozdaldogru@gmail.com>
2026-05-21 15:25:14 -0400

    chore: remove unused Clerk middleware file

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// Clerk is not used in this project, so remove Clerk middleware.
-
-export default function middleware() {
-  // No authentication or middleware logic needed
-  return;
-}
-
-// If you don't need any middleware, you can also delete this file.
-
